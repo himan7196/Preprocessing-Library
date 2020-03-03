b02638d194484acc9cc43435c91c422c5043ceb7
Himanshu <himan7196@gmail.com>
2020-03-03 12:18:33 +0530

    O/P in feather, Detect column data type

--- base.py
+++ base.py
@@ -1,11 +1,18 @@
-'''Add the name of the package you want to use in the setup.py file'''
-import setup
+'''Add the name of the package you want to use in the file'''
 import numpy
 import nltk
-
+import re
+import sklearn
+import numpy
+import nltk
+import regex
+    
 from nltk.tokenize.toktok import ToktokTokenizer
 from sklearn.preprocessing import StandardScaler
 from sklearn.impute import SimpleImputer
+from nltk.stem.porter import PorterStemmer
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
 # Download the required data
 # nltk.download('stopwords')
 # nltk.download('wordnet')
@@ -16,104 +23,97 @@ class Categorical:
 
 
 class Numeric():
-    def __init__(self, feature_scaling, handle_null, dataframe, columns):
+    def __init__(self, feature_scaling, handle_null, data, columns):
         if handle_null == True:
-            self.handle_null(dataframe, columns)
+            self.handle_null(data, columns)
         if feature_scaling == True:
-            self.feature_scaling(dataframe, columns)
+            self.feature_scaling(data, columns)
 
 
-    def feature_scaling(self, dataframe, columns):
+    def feature_scaling(self, data, columns):
         sc = StandardScaler()
         for item in columns:
-            dataframe[:, item:item+1] = sc.fit_transform(dataframe[:, item:item+1])
-        return dataframe
+            data[:, item:item+1] = sc.fit_transform(data[:, item:item+1])
+        return data
         
 
     
     #Currently replacing the NaN by mean
-    def handle_null(self, dataframe, columns):
+    def handle_null(self, data, columns):
         imputer = SimpleImputer(missing_values = numpy.nan, strategy = 'mean')
         for item in columns:
             item = int(item)
-            imputer = imputer.fit(dataframe[:,item:item+1])
-            dataframe[:,item:item+1] = imputer.transform(dataframe[:,item:item+1])
-        return dataframe
+            imputer = imputer.fit(data[:,item:item+1])
+            data[:,item:item+1] = imputer.transform(data[:,item:item+1])
+        return data
         
 
 
 class Text ():
-    def __init__(self, only_alpha, stem, lemma, stopword_removal, handle_null, dataframe, columns):
+    def __init__(self, only_alpha, stem, lemma, stopword_removal, handle_null, data, columns):
         if only_alpha==True:
-            self.only_alphabets(dataframe, columns)
+            self.only_alphabets(data, columns)
         if stem==True:
-            self.stemming(dataframe, columns)
+            self.stemming(data, columns)
         if lemma==True:
-            self.lemmetization(dataframe, columns)
+            self.lemmetization(data, columns)
         if stopword_removal==True:
-            self.stopword_removal(dataframe, columns)
+            self.stopword_removal(data, columns)
         if handle_null==True:
-            self.handle_null(dataframe, columns)
+            self.handle_null(data, columns)
         
 
-    def only_alphabets(self, dataframe, columns):
+    def only_alphabets(self, data, columns):
         '''How to handle nltk.download('stopwords')'''
-        import re
-        from nltk.stem.porter import PorterStemmer
-        from nltk.corpus import stopwords
         for item in columns:
-            for i in range (len(dataframe)):
-                text = re.sub('[^a-zA-Z]', ' ', str(dataframe[i:i+1,item:item+1]))
+            for i in range (len(data)):
+                text = re.sub('[^a-zA-Z]', ' ', str(data[i:i+1,item:item+1]))
                 text = re.sub(' +', ' ', text)
                 text = text.lower()
                 #text = text.split()
                 #ps = PorterStemmer()
                 #text = [ps.stem(word) for word in text if not word in set(stopwords.words('english'))]
                 #text = ' '.join(text)
-                dataframe[i:i+1, item:item+1] = text
+                data[i:i+1, item:item+1] = text
 
-        return dataframe
+        return data
 
-    def stemming(self, dataframe, columns):
-        '''Does porter stemming only as of now'''
-        from nltk.stem.porter import PorterStemmer
+    def stemming(self, data, columns):
         ps = PorterStemmer()
         for item in columns:
-            for i in range (len(dataframe)):
-                text = str(dataframe[i:i+1, item:item+1])
+            for i in range (len(data)):
+                text = str(data[i:i+1, item:item+1])
                 text = text[3:-3] #To remove the [['']] from the string
                 text = text.split()
                 text = [ps.stem(word) for word in text]
                 text = ' '.join(text)
-                dataframe[i:i+1, item:item+1] = text
+                data[i:i+1, item:item+1] = text
 
-        return dataframe
+        return data
 
-    def lemmetization(self, dataframe, columns):
-        from nltk.stem import WordNetLemmatizer
+    def lemmetization(self, data, columns):
         lemmatizer = WordNetLemmatizer()
         for item in columns:
-            for i in range (len(dataframe)):
-                text = str(dataframe[i:i+1, item:item+1])
+            for i in range (len(data)):
+                text = str(data[i:i+1, item:item+1])
                 text = text[3:-3]
                 text = text.split()
                 text = [lemmatizer.lemmatize(word) for word in text]
                 text = ' '.join(i for i in text)
-                dataframe[i:i+1, item:item+1] = text
-        return dataframe
+                data[i:i+1, item:item+1] = text
+        return data
         
-    def stopword_removal(self, dataframe, columns):
-        from nltk.corpus import stopwords
+    def stopword_removal(self, data, columns):
         for item in columns:
-            for i in range (len(dataframe)):
-                text = str(dataframe[i:i+1,item:item+1])
+            for i in range (len(data)):
+                text = str(data[i:i+1,item:item+1])
                 text = text[3:-3]
                 text = text.split()
                 text = [word for word in text if not word in set(stopwords.words('english'))]
                 text = ' '.join(text)
-                dataframe[i:i+1, item:item+1] = text
+                data[i:i+1, item:item+1] = text
 
-        return dataframe
+        return data
 
     def handle_null(self):
         pass
--- test_base.py
+++ test_base.py
@@ -2,7 +2,7 @@ import base
 import pandas as pd
 import json
 import csv
-
+import feather
 
 def read_config(file_path):
     with open(file_path) as f:
@@ -14,35 +14,64 @@ config = read_config('configuration.json')
 input_file_path = config["input_file_path"]
 numeric_columns = config["numeric_columns"]
 text_columns = config["text_columns"]
-output = config["output_file_name"]
-
-if numeric_columns:
-    for i in range(len(numeric_columns)):
-        numeric_columns[i] = int(numeric_columns[i])
-if text_columns:
-    for i in range(len(text_columns)):
-        text_columns[i] = int(text_columns[i])
+output_file_name = config["output_file_name"]
 
 
 dataset = pd.read_csv(input_file_path)
 headings = [item for item in dataset.columns]
 
+def validate_columns(numeric_columns, text_columns):
+    if numeric_columns:
+        for i in range(len(numeric_columns)):
+            numeric_columns[i] = int(numeric_columns[i])
+    else:
+        numeric_columns = []  
+        for i in range (len(headings)):
+            if dataset.dtypes[headings[i]]==float or dataset.dtypes[headings[i]]==int:
+                numeric_columns.append(i)
+
+    if text_columns:
+        for i in range(len(text_columns)):
+            text_columns[i] = int(text_columns[i])
+    else:
+        text_columns = []
+        for i in range (len(headings)):
+            if dataset.dtypes[headings[i]]==object or dataset.dtypes[headings[i]]==str:
+                text_columns.append(i)
+    
+    return numeric_columns,text_columns
+
+numeric_columns, text_columns = validate_columns(numeric_columns, text_columns)
+
+if not output_file_name:
+    output_file_name = 'processed'
+
+
+print(numeric_columns, text_columns)
+
+
+
 X = dataset.iloc[:, :].values
 
-base.Numeric(feature_scaling=config["feature_scaling"], handle_null=config["handle_null"], dataframe=X, columns=numeric_columns)
-base.Text(only_alpha=config["only_alpha"], stem=config["stem"], lemma=config["lemma"], stopword_removal=["stopword_removal"], handle_null=["handle_null_text"], dataframe=X, columns=text_columns)
+base.Numeric(feature_scaling=config["feature_scaling"], handle_null=config["handle_null"], data=X, columns=numeric_columns)
+base.Text(only_alpha=config["only_alpha"], stem=config["stem"], lemma=config["lemma"], stopword_removal=["stopword_removal"], handle_null=["handle_null_text"], data=X, columns=text_columns)
 
+'''feather.write_dataframe(dataset, output_file_name+'.feather')
 
+z = pd.read_feather(output_file_name+'.feather')'''
 
-print('\n\n\n\n')
-
-with open("processed.csv", 'w', newline='') as file:
+'''with open(output_file_name, 'w', newline='') as file:
     writer = csv.writer(file)
     writer.writerow(headings)
-    writer.writerows(X)
+    writer.writerows(X)'''
+
+dataframe = pd.DataFrame(data=X[:,:], columns=headings)
+
+feather.write_dataframe(dataframe, output_file_name)
+print(dataframe)
+
 
 '''
-
 #Specify the column number of your dataframe in a list
 
 print('X before: ', X)
